601cedae0e905f5fb760758ee43db5210266cb39
Muzammal Abbas <abmuzammal17@gmail.com>
2025-09-02 03:03:44 +0500

    password email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,22 +1,34 @@
 class UserMailer < ApplicationMailer
 
-  def password_email(user, password)
-    @user = user
-    @password = password
+  # def password_email(user, password)
+  #   @user = user
+  #   @password = password
 
-    mail(
-      to: @user.email,
-      subject: 'Welcome to Juicy Meets - Your Password'
-    )
+  #   mail(
+  #     to: @user.email,
+  #     subject: 'Welcome to Juicy Meets - Your Password'
+  #   )
+  # end
+
+  def password_email(user, password)
+    require 'resend'
+    Resend.api_key = ENV['RESEND_API_KEY']
+
+    Resend::Emails.send({
+      "from": "Juicy Meets <onboarding@resend.dev>",
+      "to": [user.email],
+      "subject": "Welcome to Juicy Meets - Your Password"
+    })
   end
 
-  def forgot_password_email(user, new_password)
-    @user = user
-    @new_password = new_password
+  def forgot_password_email(user, password)
+    require 'resend'
+    Resend.api_key = ENV['RESEND_API_KEY']
 
-    mail(
-      to: @user.email,
-      subject: 'Juicy Meets - Your New Password'
-    )
+    Resend::Emails.send({
+      "from": "Juicy Meets <onboarding@resend.dev>",
+      "to": [user.email],
+      "subject": "Juicy Meets - Your New Password"
+    })
   end
 end
